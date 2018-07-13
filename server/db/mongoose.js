459491e44eafe5a317e13bc364bfc2b33e0114e9
Shen, Andy <andyshen96@gmail.com>
2018-07-13 15:59:02 +1200

    Testing patch

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://AndyShen:c3b4ucme@ds233531.mlab.com:33531/todo-app-database');
+mongoose.connect(('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }) || 'mongodb://AndyShen:c3b4ucme@ds233531.mlab.com:33531/todo-app-database');
 
 module.exports = {
     mongoose
