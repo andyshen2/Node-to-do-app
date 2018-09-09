750e2ac9200e754ec3c82842960adf19e2f18df3
Shen, Andy <andyshen96@gmail.com>
2018-09-10 01:47:08 +1200

    commit

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var app = express();
 const port = process.env.PORT || 5000;
 app.use(express.static("client/build"));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "build", "Login.js"));
 });
 app.use(bodyParser.json());
 
