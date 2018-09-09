cffa534e4709549fcb103395b30a3b544b425993
Shen, Andy <andyshen96@gmail.com>
2018-09-10 01:18:44 +1200

    commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var { authenticate } = require("./middleware/authenticate");
 var app = express();
 
 const port = process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/src")));
 app.use(bodyParser.json());
 
 app.post("/todos", authenticate, (req, res) => {
